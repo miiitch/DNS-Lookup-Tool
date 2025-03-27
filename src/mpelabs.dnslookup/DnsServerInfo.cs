b7c2426958ee6c033c9d45c28be1288ee63c249f
Michel Perfetti <miiitch@gmail.com>
2025-03-27 22:40:33 +0100

    Add default server

--- src/mpelabs.dnslookup/DnsServerInfo.cs
+++ src/mpelabs.dnslookup/DnsServerInfo.cs
@@ -100,6 +100,12 @@ public class DnsServerInfo
                         },
                     ];
                 case "default":
+                    return [ new DnsServerInfo()
+                    {
+                        Name = "Default Server",
+                        ServerIpAddress = ""
+                    },];
+                default:
                     return [];
             }
         }
@@ -117,28 +123,5 @@ public class DnsServerInfo
         }
 
         return [];
-    }
-}
-
-public class DnsConfiguration
-{
-    public List<DnsServerInfo> Servers { get; set; }
-
-    public DnsConfiguration()
-    {
-        Servers = new List<DnsServerInfo>();
-    }
-    
-    
-
-    public static DnsConfiguration Load(string str)
-    {
-        var serverStr = str.Split(';', StringSplitOptions.RemoveEmptyEntries);
-
-        return new DnsConfiguration()
-        {
-            Servers = serverStr.SelectMany(DnsServerInfo.Load).ToList()
-        };
-
     }
 }
